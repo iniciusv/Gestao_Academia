d3960190d5ffc8aeec391d75031d4a2f9741dc4c
Vinicius Fortes <vinicius_fortes@outlook.com>
2024-09-04 18:53:21 -0300

    Add project files.

---
+++ Gestao_Academia/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Gestao_Academia.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Gestao_Academia.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
