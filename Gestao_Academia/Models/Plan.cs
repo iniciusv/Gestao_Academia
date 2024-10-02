a70095f6e1b342aafd972e7eb8701409c73201f8
Douglas Evangelista dos Santos <des212425@gmail.com>
2024-10-02 11:41:24 -0300

    fix: Ajuste no DTO de plano

--- Gestao_Academia/Models/Plan.cs
+++ Gestao_Academia/Models/Plan.cs
@@ -5,6 +5,11 @@ namespace Gestao_Academia.Models;
 [Table("plan")]
 public class Plan{
 	public int Id { get; set; }
-	public string? Name { get; set; }
-	public decimal Price { get; set; }
+ 	public int IdCustomer { get; set; }
+	public string? Plan_Type { get; set; }
+ 	public DateTime? Start_Date { get; set; }
+  	public DateTime? End_Date { get; set; }
+   	public Enum? Status { get;set }
+ 	public DateTime? Created_at { get; set; }
+  	public DateTime? Updated_at { get; set; }
 }
